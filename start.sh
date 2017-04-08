5881d9085ecb3829ec7398ae0b1958aec0b65231
benoitguigal <benoit.guigal@gmail.com>
2017-04-08 16:49:51 +0200

    do not use supervisord

--- start.sh
+++ start.sh
@@ -4,8 +4,4 @@ echo './start.sh'
 
 modprobe bcm2835-v4l2
 
-mkdir -p /data/log
-
-# Launch supervisor in the foreground
-echo 'Starting supervisor'
-supervisord --nodaemon --configuration /etc/supervisord.conf
+python -m cameratrap
